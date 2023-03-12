c1097a970849a0ccb76c8d08a841141d77c73c8f
Kika <kris.opletalova@gmail.com>
2023-03-12 19:42:36 +0100

    nastavování statu Climate

--- src/components/Climate/index.jsx
+++ src/components/Climate/index.jsx
@@ -1,20 +1,24 @@
 import temp from "../../images/temp.svg";
-import React from "react";
+import React, {useState} from "react";
 
-const Climate = () => {
+const Climate = ({temperature, humidity}) => {
+    const [innerTemperature, setInnerTemperature] = useState(temperature);
+
     return (
         <div className="climate">
             <div className="climate__icon">
                 <img src={temp}/>
             </div>
             <div className="climate__content">
-                <div className="climate__temperature">24&deg;C</div>
-                <div className="climate__humidity">Vlhost vzduchu 51&nbsp;%</div>
+                <div className="climate__temperature">{innerTemperature}°C</div>
+                <div className="climate__humidity">Vlhkost vzduchu {humidity} %</div>
             </div>
             <div className="climate__controls">
-                <button className="button">+</button>
-                <button className="button">-</button>
+                <button className="button" onClick={() => setInnerTemperature(innerTemperature + 1)}>+</button>
+                <button className="button" onClick={() => setInnerTemperature(innerTemperature - 1)}>-</button>
             </div>
         </div>
     )
 }
+
+export default Climate;
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Lights from "../Lights";
+import Climate from "../Climate";
 
 const Dashboard = ({data}) => {
     return (
         <main className="dashboard">
             <Lights lights={data.lights}/>
+            <Climate temperature={data.climate.temperature} humidity={data.climate.humidity} />
         </main>
     )
 }
