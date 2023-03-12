c20e7b61cd1b55b60979a15b279a676c1c15d921
Kika <kris.opletalova@gmail.com>
2023-03-12 19:28:32 +0100

    přepínání stavu Light

--- src/components/Light/index.jsx
+++ src/components/Light/index.jsx
@@ -1,17 +1,19 @@
 import lightOff from "../../images/light-off.svg";
 import lightOn from "../../images/light-on.svg";
-import React from "react";
+import React, {useState} from "react";
 
 const Light = ({state, name}) => {
-    return(
-        <div className="light">
-            <div className="light__icon">
-                <img src={lightOff}/>
-            </div>
-            <div className="light__name">
-                {name}
-            </div>
+    const [bulb, setBulb] = useState(state);
+
+return (
+    <div className="light" onClick={() => {bulb === 'on' ? setBulb('off') : setBulb('on')}}>
+        <div className="light__icon">
+            <img src={bulb === 'on' ? lightOn : lightOff}/>
         </div>
-    )
+        <div className="light__name">
+            {name}
+        </div>
+    </div>
+)
 }
 export default Light;
